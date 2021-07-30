ea0a2e2c3a354bfd3935c9aad22a156f79c9af58
Hubert MUGABO <mugberto@gmail.com>
2021-07-30 02:35:08 +0200

    Style alert message

--- src/js/messageWidget.js
+++ src/js/messageWidget.js
@@ -3,9 +3,10 @@ const showResponseMsg = (message) => {
   const firstInput = document.getElementById('name');
   const p = document.createElement('p');
   p.textContent = message;
+  p.className = 'alert-msg';
   form.insertBefore(p, firstInput);
   setTimeout(() => {
     p.remove();
-  }, 5000);
+  }, 4000);
 };
 export default showResponseMsg;
